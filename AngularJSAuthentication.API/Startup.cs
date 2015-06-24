1f6bfc2efd6a64774b5c934d3748b4511d7c9566
Matheus Moreira <mvrmoreira@gmail.com>
2015-06-24 17:32:19 -0300

    Implement the “SimpleAuthorizationServerProvider” class

--- AngularJSAuthentication.API/Startup.cs
+++ AngularJSAuthentication.API/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Owin;
+﻿using AngularJSAuthentication.API.Providers;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
 using Owin;
 using System;
 using System.Collections.Generic;
@@ -13,9 +15,23 @@ namespace AngularJSAuthentication.API
     {
         public void Configuration(IAppBuilder app)
         {
-            HttpConfiguration config = new HttpConfiguration();
-            WebApiConfig.Register(config);
-            app.UseWebApi(config);
+            ConfigureOAuth(app);
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider()
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
         }
     }
 }
